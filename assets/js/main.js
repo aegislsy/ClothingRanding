73889fc9db282449ff60d77a89fe8fde5ab9cbc9
aegislsy <leesaeyim@naver.com>
2022-10-17 19:33:30 +0900

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
